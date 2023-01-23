d219dd18671b4f49b4968d9dfb8342f4cbde8ecf
golu9031 <a8015457557@gmail.com>
2023-01-23 18:35:40 +0530

    test

--- web dev/amit server/amit.js
+++ web dev/amit server/amit.js
@@ -23,6 +23,7 @@ const PORT=8080;
 fs.readFile('C:\\Users\\amita\\Desktop\\web dev\\school project\\project.html', function (err, html) {
 
     if (err) throw err;    
+    
 
     http.createServer(function(request, response) {  
         response.writeHeader(200, {"Content-Type": "text/html"});  
