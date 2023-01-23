c8aa3a7acce9d631c20b2d510297b8b9f539aa89
golu9031 <a8015457557@gmail.com>
2023-01-23 17:47:57 +0530

    this is my frirst checkin

---
+++ web dev/school project/index.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var fs = require('fs');
+
+const PORT=8080; 
+
+fs.readFile('C:\\Users\\amita\\Desktop\\web dev\\school project\\project.html', function (err, html) {
+
+    if (err) throw err;    
+
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(PORT);
+});
+fs.readFile('C:\\Users\\amita\\Desktop\\web dev\\school project\\login.html', function (err, html) {
+
+    if (err) throw err;    
+
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(PORT);
+});
